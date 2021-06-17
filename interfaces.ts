c39a6a8c9cf08339877c74a8fa55c1be15b607d3
Zorba11 <zorbathegreek111@gmail.com>
2021-06-17 20:41:40 +0530

    more on interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,30 @@
-interface Vehicle {
-  name: string,
-  year: number,
-  broken: boolean
+export {}
+
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary);
 }
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(drink);
